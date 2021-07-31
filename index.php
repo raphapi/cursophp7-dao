e82a7e6c2db8d2ff41a24aa22a9b06bda735d831
Raphael Pinheiro <francisco.raphaelpinheiro@gmail.com>
2021-07-31 10:22:39 -0300

    criada a classe usuario

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php 
 
-	require_once ("config.php");
-
+	require_once("config.php");
+	/* SELECT:
 	$sql = new Sql();
 
 	$usuarios = $sql->select("SELECT *  FROM  tb_usuario");
 
-	echo json_encode($usuarios);
+	echo json_encode($usuarios);*/
+	
+	$user = new Usuario();
+
+	$user->loadbyId(5);
+
+	echo $user;
+
  ?>
